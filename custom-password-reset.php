453a0a3d356ba572ff10e4605878174a119e39ad
klhall1987@gmail.com <Kenneth@wpninjas.com>
2015-11-23 11:03:02 -0500

    initial commit.

---
+++ custom-password-reset.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Custom Password Reset Email
+Description: Customize the text in the default WordPress password reset email.
+Version: 0.0.1
+Author: Kenny Hall
+Author URI: http://kennyinthewild.com
+Text Domain: custom-password-reset
+*/
+
+class Custom_Password_Reset_Email
+{
+
+
+
+}
