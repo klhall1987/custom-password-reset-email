e0a707436d1015e0aa6dcf92fa76e7e507b1ddd8
klhall1987@gmail.com <Kenneth@wpninjas.com>
2015-12-07 15:56:24 -0500

    Changed a line of text.

--- includes/cpr-admin-template.html.php
+++ includes/cpr-admin-template.html.php
@@ -21,7 +21,7 @@
                 Message:<br>
                 <textarea rows="4" cols="30" name="cpr-message"><?php echo get_option( 'cpr_message_option' ); ?></textarea>
             </label>
-            <h5>Insert {password_key} shortcode where ever you would like the key to appear in your email.</h5>
+            <h5>Insert {password_key} shortcode where ever you would like the email password key to appear in your email.</h5>
             <label>
                 <input type="submit" name="cpr-submit">
             </label>
